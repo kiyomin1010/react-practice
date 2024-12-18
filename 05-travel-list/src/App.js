58994400be423a4d3f758c4ef0e10f7ab5455aab
kiyomin0615 <moeday1010@gmail.com>
2024-12-18 15:33:51 +0900

    05-add derived state which are calculated based on existing state

--- 05-travel-list/src/App.js
+++ 05-travel-list/src/App.js
@@ -32,7 +32,7 @@ export default function App() {
         onDeleteItem={deleteItem}
         onToggleItem={toggleItem}
       />
-      <Stats />
+      <Stats items={items} />
     </div>
   );
 }
@@ -132,10 +132,26 @@ function Item({ item, onDeleteItem, onToggleItem }) {
   );
 }
 
-function Stats() {
+function Stats({ items }) {
+  if (!items.length)
+    return (
+      <footer className="stats">
+        <em>Start adding some items to your packing list.</em>
+      </footer>
+    );
+
+  const numItems = items.length;
+  const numPackedItems = items.filter((item) => item.packed).length;
+  const percentage = Math.round((numPackedItems / numItems) * 100);
+
   return (
     <footer className="stats">
-      <em>You have X items on your list, and you already packed X (X%)</em>
+      <em>
+        {percentage === 100
+          ? "You got everything! Ready to go! ✈️"
+          : `You have ${numItems} items on your list, and you already packed
+        ${numPackedItems} (${percentage}%)`}
+      </em>
     </footer>
   );
 }
