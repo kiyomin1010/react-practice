1a4b47a2349faacc6635a9cbdb5d1f486f969e8e
kiyomin0615 <moeday1010@gmail.com>
2025-01-04 11:13:34 +0900

    07-use ref to select dom elements in React way

--- 07-search-movie/src/components/Search.js
+++ 07-search-movie/src/components/Search.js
@@ -1,4 +1,27 @@
+import { useEffect, useRef } from "react";
+
 export default function Search({ query, setQuery }) {
+  const inputEl = useRef(null);
+
+  useEffect(() => {
+    function eventListener(e) {
+      if (document.activeElement === inputEl.current) return;
+
+      if (e.code === "Enter") {
+        inputEl.current.focus();
+        setQuery("");
+      }
+    }
+
+    document.addEventListener("keydown", eventListener);
+
+    inputEl.current.focus();
+
+    return () => {
+      document.removeEventListener("keydown", eventListener);
+    };
+  }, [setQuery]);
+
   return (
     <input
       className="search"
@@ -8,6 +31,7 @@ export default function Search({ query, setQuery }) {
       onChange={(e) => {
         setQuery(e.target.value);
       }}
+      ref={inputEl}
     />
   );
 }
