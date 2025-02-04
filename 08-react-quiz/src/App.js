2adcabd0beb7d352ce96e8d15af9175844520350
kiyomin0615 <moeday1010@gmail.com>
2025-02-04 11:02:21 +0900

    08-start quiz and display question and options

--- 08-react-quiz/src/App.js
+++ 08-react-quiz/src/App.js
@@ -5,9 +5,11 @@ import Main from "./components/Main";
 import Loader from "./components/Loader";
 import Error from "./components/Error";
 import Start from "./components/Start";
+import Question from "./components/Question";
 
 const initialState = {
   questions: [],
+  index: 0,
   status: "loading", // loading, error, ready, active, finished
 };
 
@@ -24,13 +26,21 @@ function reducer(state, action) {
         ...state,
         status: "error",
       };
+    case "start":
+      return {
+        ...state,
+        status: "active",
+      };
     default:
       throw new Error("Action unknown");
   }
 }
 
 export default function App() {
-  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+  const [{ questions, index, status }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
 
   const questionCount = questions.length;
 
@@ -47,7 +57,10 @@ export default function App() {
       <Main>
         {status === "loading" && <Loader />}
         {status === "error" && <Error />}
-        {status === "ready" && <Start questionCount={questionCount} />}
+        {status === "ready" && (
+          <Start questionCount={questionCount} dispatch={dispatch} />
+        )}
+        {status === "active" && <Question question={questions[index]} />}
       </Main>
     </div>
   );
--- 08-react-quiz/src/components/Start.js
+++ 08-react-quiz/src/components/Start.js
@@ -1,11 +1,16 @@
-export default function Start({ questionCount }) {
+export default function Start({ questionCount, dispatch }) {
   return (
     <div className="start">
       <h2>Welcome to The React Quiz!</h2>
-      <h3>
-        We have {questionCount} questions for testing your React knowledge
-      </h3>
-      <button className="btn btn-ui">Start</button>
+      <h3>{questionCount} questions</h3>
+      <button
+        className="btn btn-ui"
+        onClick={() => {
+          dispatch({ type: "start" });
+        }}
+      >
+        Start
+      </button>
     </div>
   );
 }
